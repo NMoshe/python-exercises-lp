0fd1cff5ffd7899f53e2967c57af7a143ff1d62e
Moshe <nicholas.namrot@gmail.com>
2020-07-20 21:38:44 -0400

    diceware added

--- Password_Generator.py
+++ Password_Generator.py
@@ -2,6 +2,7 @@
 # python 3.8
 import random
 import string
+import time
 
 
 def openFile(file):
@@ -46,5 +47,44 @@ def complexPassPhrase(words):
 
     return print(passphrase)
 
+
+def dicewareDiceroll():
+    numberStr = ""
+    print("Rolling the die for your 5 number combination...")
+    for x in range(5):
+        roll = random.randint(1, 6)
+        numberStr += str(roll)
+        time.sleep(random.randint(1, 3))
+        if x == 0:
+            print(f"First roll...{roll}!")
+        elif x == 1:
+            print(f"Second roll...{roll}!")
+        elif x == 2:
+            print(f"Third roll...{roll}!")
+        elif x == 3:
+            print(f"Fourth roll...{roll}!")
+        else:
+            print(f"Fifth roll...{roll}!")
+
+    return int(numberStr)
+
+
+def dicewarePassPhrase(diceware):
+    dicewareDict = {}
+    dicewarePass = ""
+    with open(diceware, "r") as of:
+        for x in of:
+            x.split
+            code, word = x[:6], x[6:]
+            dicewareDict[code.replace("\t", "")] = word.replace("\n", "")
+
+    for x in range(6):
+        code = dicewareDiceroll()
+        print(f"Your code {code} returned the word {dicewareDict[str(code)]}")
+        dicewarePass += dicewareDict[str(code)] + " "
+
+    return print(dicewarePass)
+
 createPassPhrase(openFile("english.txt"))
 complexPassPhrase(openFile("english.txt"))
+dicewarePassPhrase("diceware.txt")
